5f48e3de65d196280da442f28cdd5e46adb3060d
yuananwu <wuyuanan13@gmail.com>
2024-02-29 16:01:25 +0800

    update

--- R/kappa.R
+++ R/kappa.R
@@ -1,61 +1,58 @@
 library("raters")
-#将原始数据转化为频次数据
-freq_stat_2levels = function(df){#数据转换,每一个样本转换为所有评分者结果的0,1频次集合
-  freq_vec = vector(mode="numeric",length=2*nrow(df))
-  i = 1
-  while(i <= nrow(df)){
-    j = 1
-    while(j <= ncol(df)){
-      if( df[i,j] == 0 ){
-        freq_vec[2*i-1] = freq_vec[2*i-1]+1
-      }else{
-        freq_vec[2*i] = freq_vec[2*i]+1
-      }
-      j = j+1
-    }
-    i = i+1
+
+isDfContainNa = function(df){
+  uniqueEle = unique(unlist(df))
+  if( TRUE %in% is.na(uniqueEle)){
+    return(TRUE)
+  }else{
+    return(FALSE)
   }
-  return(matrix(freq_vec,ncol = 2,byrow = T))
 }
 
-#数据转换,每一个样本转换为所有评分者结果的0,1,2频次集合
-freq_stat_3levels = function(df){
-  freq_vec = vector(mode="numeric",length=3*nrow(df))
-  i = 1
-  while(i <= nrow(df)){
-    j = 1
-    while(j <= ncol(df)){
-      if( df[i,j] == 3 ){
-        freq_vec[3*i-2] = freq_vec[3*i-2]+1
-      }else if( df[i,j] == 4 ){
-        freq_vec[3*i-1] = freq_vec[3*i-1]+1
-      }else {
-        freq_vec[3*i  ] = freq_vec[3*i  ]+1
-      }
-      j = j+1
-    }
-    i = i+1
+# 将多位raters的评分数据, 转化为评分的频数
+df2Freq = function(df, na.rm = F){
+  uniqueFactor = sort(unique(unlist(df)))
+  fNum = length(uniqueFactor)
+  if (fNum == 1){
+    stop('unique factor number equal 1')
   }
-  return(matrix(freq_vec,ncol = 3,byrow = T))
+  if (isDfContainNa(df)){
+    if(na.rm){
+      df = na.omit(df)
+      cat(names(df), '存在空值, 计算过程会自动将空值剔除掉, 剩余样本量: ', dim(df)[1], '\n')
+    }else{
+      stop('missing values exist')
+    }
+  }
+  rst = NULL
+  for (i in 1:nrow(df)){
+    rstTmp = rep(0,fNum)
+    for (j in 1:ncol(df)){
+      for (idx in 1:fNum){
+        if(df[i,j] == uniqueFactor[idx]){
+          rstTmp[idx] = rstTmp[idx] + 1
+        }
+      }
+    }
+    rst = rbind(rst, rstTmp)
+  }
+  return(as.matrix.data.frame(rst))
 }
 
-
-fleissKappa = function(df){
+# 当评分的level只有两个时, 调用该方法
+getKappaFor2Levels = function(df){
   # Monte Carlo检验方法用来计算Fleiss Kappa的95CI,适用于小数据集
   #检验方法选择参考: https://cran.r-project.org/web/packages/raters/raters.pdf
-  f_k_rsl = concordance(df,test="MC",B=100,alpha = 0.05)
-  print(paste(
-    " Fleiss Kappa :", round(f_k_rsl$Fleiss[1],3),
-    " (",           round(f_k_rsl$Fleiss[2],3),
-    "-",               round(f_k_rsl$Fleiss[3],3), ") " ,sep = ""))
+  kp = concordance(df,test="MC",B=100,alpha = 0.05)
+  value = kp$Fleiss
+  rst = paste(round(value[1], 3), ' (',round(value[2], 3), '-', round(value[3], 3), ')',   sep = '')
+  return(rst)
 }
 
-#这是多raters&多levels的计算方法
-fleissKappaMultiLevels = function(df){
+# 当评分的level大于两个时, 调用该方法
+getKappaForMultiLevels = function(df){
   set.seed(42)
-  result_a = wlin.conc(df,test="MC",B=25)
-  print(paste(
-    " Kappa :", round(result_a[1],3),
-    " (",           round(result_a[3],3),
-    "-",               round(result_a[4],3), ") " ,sep = ""))
+  kp = wlin.conc(df,test="MC",B=30)
+  rst = paste(round(kp[1], 3), ' (',round(kp[3], 3), '-', round(kp[4], 3), ')',   sep = '')
+  return(rst)
 }
